35c99857a5b666fa71aa2ece1a3f67eeab46cdc2
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-27 21:53:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,8 @@ end
 def batch_badge_creator(attendees)
   
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
+attendees.each_with_index do |names, index|
+  puts "Hello my name is #{name}"
 end 
 
 def assign_rooms
